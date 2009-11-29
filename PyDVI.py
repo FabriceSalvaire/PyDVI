640cc3118ebc3399f6c36111d9c37fe20dfd091c
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2009-11-29 18:55:59 +0100

    postamble

--- PyDVI.py
+++ PyDVI.py
@@ -7,7 +7,20 @@ DVI_FORMAT = 2
 DVIV_FORMAT = 3
 XDVI_FORMAT = 5
 
+NOP_OPCODE = 138
+FNT_DEF1_OPCODE = 243
+FNT_DEF2_OPCODE = 244
+FNT_DEF3_OPCODE = 245
+FNT_DEF4_OPCODE = 246
 PRE_OPCODE = 247
+POST_OPCODE = 248
+POST_POST_OPCODE = 249
+
+EOF_SIGNATURE = 223
+
+SEEK_RELATIVE_TO_START   = 0
+SEEK_RELATIVE_TO_CURRENT = 1
+SEEK_RELATIVE_TO_END     = 2
 
 # opcodes =
 
@@ -29,6 +42,10 @@ class DviProcessor(object):
         self.magnification = 1000
         self.comment       = ''
 
+        self.bop_pointer_stack = []
+
+        self.fonts = {}
+
     ###############################################
 
     def process_stream(self, stream):
@@ -38,6 +55,7 @@ class DviProcessor(object):
         self.reset()
 
         self.process_preambule()
+        self.process_postambule()
 
     ###############################################
 
@@ -50,7 +68,7 @@ class DviProcessor(object):
 
         return self.stream.read(n)
 
-    def get_big_endian_number(self, n, signed = False):
+    def read_big_endian_number(self, n, signed = False):
 
         '''Read a number coded in big endian format from the DVI input stream
 
@@ -70,48 +88,128 @@ class DviProcessor(object):
 
         return number
             
-    def get_unsigned_byte1(self):
-        return self.get_big_endian_number(1, signed = False)
+    def read_unsigned_byte1(self):
+        return self.read_big_endian_number(1, signed = False)
 
-    def get_signed_byte1(self):
-        return self.get_big_endian_number(1, signed = True)
+    def read_signed_byte1(self):
+        return self.read_big_endian_number(1, signed = True)
 
-    def get_unsigned_byte2(self):
-        return self.get_big_endian_number(2, signed = False)
+    def read_unsigned_byte2(self):
+        return self.read_big_endian_number(2, signed = False)
 
-    def get_signed_byte2(self):
-        return self.get_big_endian_number(2, signed = True)
+    def read_signed_byte2(self):
+        return self.read_big_endian_number(2, signed = True)
 
-    def get_unsigned_byte3(self):
-        return self.get_big_endian_number(3, signed = False)
+    def read_unsigned_byte3(self):
+        return self.read_big_endian_number(3, signed = False)
 
-    def get_signed_byte3(self):
-        return self.get_big_endian_number(3, signed = True)
+    def read_signed_byte3(self):
+        return self.read_big_endian_number(3, signed = True)
 
-    def get_unsigned_byte4(self):
-        return self.get_big_endian_number(4, signed = False)
+    def read_unsigned_byte4(self):
+        return self.read_big_endian_number(4, signed = False)
 
-    def get_signed_byte4(self):
-        return self.get_big_endian_number(4, signed = True)
+    def read_signed_byte4(self):
+        return self.read_big_endian_number(4, signed = True)
 
     ###############################################
 
     def process_preambule(self):
 
-        # self.stream.seek(0)
+        self.stream.seek(0)
 
-        if self.get_unsigned_byte1() != PRE_OPCODE:
+        if self.read_unsigned_byte1() != PRE_OPCODE:
             raise NameError('Bad DVI stream')
 
-        self.dvi_id = self.get_unsigned_byte1()
+        self.dvi_id = self.read_unsigned_byte1()
         if self.dvi_id not in (DVI_FORMAT, DVIV_FORMAT, XDVI_FORMAT):
             raise NameError('Bad DVI Format')
 
-        self.numerator     = self.get_unsigned_byte4()
-        self.denominator   = self.get_unsigned_byte4()
-        self.magnification = self.get_unsigned_byte4()
+        self.numerator     = self.read_unsigned_byte4()
+        self.denominator   = self.read_unsigned_byte4()
+        self.magnification = self.read_unsigned_byte4()
 
-        self.comment = self.read_stream(self.get_unsigned_byte1())
+        self.comment = self.read_stream(self.read_unsigned_byte1())
+
+    ###############################################
+
+    def process_postambule(self):
+
+        # DVI file end with at least four EOF_SIGNATURE
+        
+        # Test -5 byte
+
+        opcode = NOP_OPCODE
+
+        self.stream.seek(-5, SEEK_RELATIVE_TO_END)
+
+        while True:
+
+            opcode = self.read_unsigned_byte1()
+
+            if opcode < 0: # found EOF
+                raise NameError('Bad DVI stream')
+            elif opcode != EOF_SIGNATURE:
+                break
+
+            self.stream.seek(-2, SEEK_RELATIVE_TO_CURRENT)
+
+        dvi_format = opcode
+
+        self.stream.seek(-5, SEEK_RELATIVE_TO_CURRENT)
+        self.post_pointer = self.read_unsigned_byte4()
+
+        # Move to post
+        self.stream.seek(self.post_pointer)
+
+        if self.read_unsigned_byte1() != POST_OPCODE:
+            raise NameError('Bad DVI stream')            
+
+        self.bop_pointer_stack.append(self.read_unsigned_byte4())
+
+        numerator     = self.read_unsigned_byte4()
+        denominator   = self.read_unsigned_byte4()
+        magnification = self.read_unsigned_byte4()
+
+        self.max_height = self.read_unsigned_byte4()
+        self.max_width  = self.read_unsigned_byte4()
+        self.stack_depth = self.read_unsigned_byte2()
+        self.number_of_pages = self.read_unsigned_byte2()
+
+        # Read Font definition
+
+        while True:
+
+            opcode = self.read_unsigned_byte1()
+
+            if   opcode == FNT_DEF1_OPCODE: font_id = self.read_unsigned_byte1()
+            elif opcode == FNT_DEF2_OPCODE: font_id = self.read_unsigned_byte2()
+            elif opcode == FNT_DEF3_OPCODE: font_id = self.read_unsigned_byte3()
+            elif opcode == FNT_DEF4_OPCODE: font_id = self.read_unsigned_byte4()
+            elif opcode != NOP_OPCODE: break
+
+            self.define_font(font_id)
+
+        if opcode != POST_POST_OPCODE:
+            raise NameError('Bad DVI stream')
+
+        post_pointer = self.read_unsigned_byte4()
+        dvi_format = self.read_unsigned_byte1()
+
+    ###############################################
+
+    def define_font(self, font_id):
+
+        font_checksum     = self.read_unsigned_byte4()
+        font_scale_factor = self.read_unsigned_byte4()
+        font_design_size  = self.read_unsigned_byte4()
+        
+        font_name = self.read_stream(self.read_unsigned_byte1() + self.read_unsigned_byte1())
+
+        self.fonts[font_id] = {'name':font_name,
+                               'checksum':font_checksum,
+                               'scale_factor':font_scale_factor,
+                               'design_size':font_design_size}
 
     ###############################################
 
@@ -119,12 +217,25 @@ class DviProcessor(object):
 
         print '''
 Preambule
-  - DVI Format    %u
-  - Numerator     %u
-  - Denominator   %u
-  - Magnification %u
-  - Comment       '%s'
-''' % (self.dvi_format, self.numerator, self.denominator, self.magnification, self.comment)
+  - DVI format    %u
+  - numerator     %u
+  - denominator   %u
+  - magnification %u
+  - comment       '%s'
+
+Postamble
+  - pointer         %u
+  - max height      %u
+  - max width       %u
+  - stack depth     %u
+  - number of pages %u
+
+Fonts
+''' % (self.dvi_format, self.numerator, self.denominator, self.magnification, self.comment,
+       self.post_pointer, self.max_height, self.max_width, self.stack_depth, self.number_of_pages)
+
+        for font_id in self.fonts.keys():
+            print self.fonts[font_id]
 
 #####################################################################################################
 #
