c08a9678f8e41518ab45d34784c978b093decc76
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2014-09-04 14:11:16 +0200

    GUI cleanup

--- PyDviGui/FontViewer/MainWindow.py
+++ PyDviGui/FontViewer/MainWindow.py
@@ -8,16 +8,6 @@
 ####################################################################################################
 
 ####################################################################################################
-#
-# Audit
-#
-# - 10/12/2011
-# 
-####################################################################################################
-
-####################################################################################################
-
-import math
 
 from PyQt4 import QtGui, QtCore
 
@@ -25,18 +15,15 @@ from PyQt4 import QtGui, QtCore
 
 from PyDVI.FontManager import FontManager
 
-from FontInfoTableModel import FontInfoTableModel
-from GlyphInfoTableModel import GlyphInfoTableModel
-from MainWindowBase import MainWindowBase
+from ..MainWindowBase import MainWindowBase
+from .FontInfoTableModel import FontInfoTableModel
+from .GlyphInfoTableModel import GlyphInfoTableModel
 
 ####################################################################################################
 
-from font_viewer_ui import Ui_main_window
+from ..ui.font_viewer_ui import Ui_main_window
 
 ####################################################################################################
-#
-# Main Window
-#
 
 class MainWindow(MainWindowBase):
 
@@ -50,26 +37,14 @@ class MainWindow(MainWindowBase):
 
         self.main_window_ui = Ui_main_window()
         self.main_window_ui.setupUi(self)
-
-        super(MainWindow, self).init_actions()
-        self.__init_actions()
-        self.__init_menu()
-
-        # Init Signals
-
         form = self.main_window_ui
 
-        signal = QtCore.SIGNAL('clicked()')
+        super(MainWindow, self).init_actions()
 
-        QtCore.QObject.connect(form.load_font_button, signal, self.load_font)
-        QtCore.QObject.connect(form.pk_radio_button, signal, self.init_font_manager)
-        QtCore.QObject.connect(form.type1_radio_button, signal, self.init_font_manager)
-
-        signal = QtCore.SIGNAL('valueChanged(int)')
-
-        QtCore.QObject.connect(form.char_code_spin_box, signal, self.show_glyph)
-
-        # Init Form
+        form.load_font_button.clicked.connect(self.load_font)
+        form.pk_radio_button.clicked.connect(self.init_font_manager)
+        form.type1_radio_button.clicked.connect(self.init_font_manager)
+        form.char_code_spin_box.valueChanged.connect(self.show_glyph)
 
         if args.font_name is not None:
             form.font_name_line_edit.setText(args.font_name)
@@ -83,33 +58,13 @@ class MainWindow(MainWindowBase):
         self.init_font_manager()
 
     ##############################################
-    #
-    # Menu
-    #
-
-    def __init_menu(self):
-
-        form = self.main_window_ui
-
-    ##############################################
-    #
-    # Actions
-    #
-
-    def __init_actions(self):
-
-        form = self.main_window_ui
-
-    ##############################################
 
     def init_font_manager(self):
 
         form = self.main_window_ui
 
-        self.font_manager = FontManager(font_map = 'pdftex', use_pk = form.pk_radio_button.isChecked())
-
+        self.font_manager = FontManager(font_map='pdftex', use_pk=form.pk_radio_button.isChecked())
         form.font_information_table_view.resizeColumnsToContents()
-
         self.load_font()
 
     ##############################################
@@ -119,21 +74,17 @@ class MainWindow(MainWindowBase):
         form = self.main_window_ui
 
         font_name = str(form.font_name_line_edit.text())
-
         if not font_name:
             return
 
         #try:
+
         # self.font = self.font_manager.load_font(font_types.Pk, font_name)
         self.font = self.font_manager[font_name]
-
         self.font_information_table_model.set_font(self.font)
         form.font_information_table_view.resizeColumnsToContents()
-        
         form.char_code_spin_box.setMaximum(len(self.font) -1)
-        
         # self.font.print_summary()
-        
         self.show_glyph(0)
 
         #except:
