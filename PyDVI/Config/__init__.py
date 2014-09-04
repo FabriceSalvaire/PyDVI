cdaa7909173b85247cb62839a40ec7144dc1c1a5
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2014-09-04 14:25:51 +0200

    Updated file headers

--- PyDVI/Config/__init__.py
+++ PyDVI/Config/__init__.py
@@ -1,6 +1,6 @@
 ####################################################################################################
 # 
-# PyDVI - A Python Library to Process DVI Stream.
+# PyDVI - A Python Library to Process DVI Stream
 # Copyright (C) 2014 Fabrice Salvaire
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,6 +14,6 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # 
 ####################################################################################################
